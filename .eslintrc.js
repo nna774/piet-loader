3523e39acc1a8051e5ea8e03fe9c88837b2e5126
NoNameA 774 <nonamea774@gmail.com>
2016-09-29 04:45:31 +0900

    install eslint

---
+++ .eslintrc.js
@@ -0,0 +1,17 @@
+module.exports = {
+  "extends": "airbnb",
+  "rules": {
+    "no-console": "off",
+    "no-mixed-operators": "off",
+    "no-bitwise": "off",
+    "no-plusplus": "off",
+    "no-continue": "off",
+    "yoda": ["error", "never", { "onlyEquality": true }],
+    "strict": ["error", "function"],
+    "arrow-parens": ["error", "always"],
+  },
+  env: {
+    node: true,
+    browser: false
+  },
+}
